a05c42007a547cce56bd8698650c3630a186a359
NikhilSHarma8429 <97504900+NikhilSHarma8429@users.noreply.github.com>
2024-06-03 18:52:35 +0530

    Completed Resume Page

--- src/assets/Components/Resume/Resume.jsx
+++ src/assets/Components/Resume/Resume.jsx
@@ -6,7 +6,7 @@ function Resume() {
     <>
       <div className="content-container">
         <div className="header-container" id={ResumeCSS.headerContainer}>
-          <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px', width: '80%', margin: 'auto',color: '#a5b3ce' }}>
+          <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px', paddingBottom:'0', width: '80%', margin: 'auto',color: '#a5b3ce' }}>
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <div>
             <h1 style={{ margin: '0', color:'white' }}>Nikhil Sharma</h1>
@@ -16,12 +16,12 @@ function Resume() {
           </div>
         </div>
         <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
-          <a href="https://github.com/NikhilSHarma8429" target="_blank"style={{ marginRight: '10px' }}>GitHub</a>
-          <a href="https://www.linkedin.com/in/nikhil-sharma-4a599221b/" target="_blank"style={{ marginRight: '10px' }}>LinkedIn</a>
-          <a href="https://leetcode.com/u/sharma842nikhil/" target="_blank">LeetCode</a>
+          <a href="https://github.com/NikhilSHarma8429" target="_blank" style={{ marginRight: '10px', textDecoration:'none', color:'#fe655c'}}>GitHub <i class="fa-solid fa-link"></i></a>
+          <a href="https://www.linkedin.com/in/nikhil-sharma-4a599221b/" target="_blank" style={{ marginRight: '10px', textDecoration:'none', color:'#fe655c'}}>LinkedIn <i class="fa-solid fa-link"></i></a>
+          <a href="https://leetcode.com/u/sharma842nikhil/" target="_blank" style={{textDecoration:'none', color:'#fe655c'}}>LeetCode <i class="fa-solid fa-link"></i></a>
         </div>
-        <h2 style={{color: 'white'}}>Education</h2>
-        <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+        <h2 style={{color: 'white', paddingTop:'1rem'}}>Education</h2>
+        <table style={{ width: '100%', borderCollapse: 'collapse', marginTop:'0.5rem'}}>
           <thead>
             <tr>
               <th style={{ border: '1px solid black', padding: '8px', color:'white' }}>Degree/Certificate</th>
@@ -51,11 +51,14 @@ function Resume() {
             </tr>
           </tbody>
         </table>
-        <h2 style={{color: 'white'}}>Projects</h2>
+        <h2 style={{color: 'white', paddingTop:'1rem'}}>Projects</h2>
         <div>
-          <h3 style={{color: 'white'}}>Social Media Comments Sentiment Analyzer (Ongoing)</h3>
+          <div style={{display:'flex', color: 'white', paddingTop:'0.5rem'}}>
+          <h3 style={{}}>Social Media Comments Sentiment Analyzer </h3>
+          <h3 style={{width:'max-content', paddingLeft:'0.2rem', fontSize:'1rem', color:'#fe655c', display:'flex', alignItems:'center'}}>(Ongoing)</h3>
+          </div>
           <p>Enhanced content quality by providing sentiment-based engagement insights between audience and creator.</p>
-          <ul>
+          <ul style={{paddingTop:'0.3rem'}}>
             <li>Analyze social media comments into different categories and provide ratio of them, comment categories are like Positive Comment, Negative Comment, Neutral Comment.</li>
             <li>Through Machine Learning and Artificial Intelligence provide suggestions for improving the content.</li>
             <li>Processed and analyzed over 10,000 comments during the development and testing phases.</li>
@@ -63,9 +66,9 @@ function Resume() {
           </ul>
         </div>
         <div>
-          <h3 style={{color: 'white'}}>FitSportChamp Website <a href="https://fitsportchamp.vercel.app/">(Live URL)</a> | <a href="https://github.com/NikhilSHarma8429/fitsportchamp">Github</a></h3>
+          <h3 style={{color: 'white', paddingTop:'0.5rem'}}>FitSportChamp Website <a href="https://fitsportchamp.vercel.app/" target="_blank" style={{color:'#fe655c', textDecoration:'none',fontSize:'1rem'}}>Live URL <i class="fa-solid fa-link"></i></a> | <a href="https://github.com/NikhilSHarma8429/fitsportchamp" target="_blank" style={{color:'#fe655c', textDecoration:'none',fontSize:'1rem'}}>Github <i class="fa-solid fa-link"></i></a></h3>
           <p>Providing users with comprehensive fitness and sports content.</p>
-          <ul>
+          <ul style={{paddingTop:'0.3rem'}}>
             <li>Developed a project for the GFG Solving for India Hackathon 2023, with participation from over 100 colleges.</li>
             <li>Offered diverse reading materials on 6+ exercises and 6+ sports to enhance user’s physical fitness and diet.</li>
             <li>Created and formatted 12+ articles and tutorials on fitness and sports, showcasing a wide range of topics.</li>
@@ -73,20 +76,20 @@ function Resume() {
           </ul>
         </div>
         <div>
-          <h3 style={{color: 'white'}}>YouTube And Spotify Clone with YouTube videos downloader <a href="https://all-in-one-vercel.vercel.app/">(Live URL)</a> | <a href="https://github.com/NikhilSHarma8429/Youtube-And-Spotify-Clone">Github</a></h3>
+          <h3 style={{color: 'white', paddingTop:'0.5rem'}}>YouTube And Spotify Clone with YouTube videos downloader <a href="https://all-in-one-vercel.vercel.app/" target="_blank" style={{color:'#fe655c', textDecoration:'none',fontSize:'1rem'}}>Live URL <i class="fa-solid fa-link"></i></a> | <a href="https://github.com/NikhilSHarma8429/Youtube-And-Spotify-Clone" target="_blank" style={{color:'#fe655c', textDecoration:'none',fontSize:'1rem'}}>Github <i class="fa-solid fa-link"></i></a></h3>
           <p>Built a multifunctional platform for video streaming, downloading, and music playback.</p>
-          <ul>
+          <ul style={{paddingTop:'0.3rem'}}>
             <li>Designed to replicate YouTube and Spotify functionalities, including video streaming and music playback.</li>
             <li>Enabled users to watch and download YouTube videos, and listen to music seamlessly, increasing user retention by 40%.</li>
             <li>Integrated YouTube Data API and Spotify API, with a user-friendly interface created using Bootstrap, leading to a 50% reduction in bounce rate.</li>
             <li>HTML, CSS, JavaScript, and BootStrap.</li>
           </ul>
         </div>
-        <h2 style={{color: 'white'}}>Key Courses Taken</h2>
+        <h2 style={{color: 'white', paddingTop:'1rem'}}>Key Courses Taken</h2>
         <ul>
           <li>Data Structure, Algorithms, DBMS, Machine Learning, Artificial Intelligence, Web Services, Operating System.</li>
         </ul>
-        <h2 style={{color: 'white'}}>Technical Skills</h2>
+        <h2 style={{color: 'white', paddingTop:'1rem'}}>Technical Skills</h2>
         <ul>
           <li><strong>Programming:</strong> C/C++, Python, JavaScript, SQL.</li>
           <li><strong>Tools & OS:</strong> Git, Jupyter Notebook, Google Colab, Windows.</li>
@@ -94,7 +97,7 @@ function Resume() {
           <li><strong>Web Skills:</strong> HTML/CSS/JS, ReactJS.</li>
           <li><strong>Databases:</strong> MySql, MongoDB.</li>
         </ul>
-        <h2 style={{color: 'white'}}>Achievements</h2>
+        <h2 style={{color: 'white', paddingTop:'1rem'}}>Achievements</h2>
         <ul>
           <li>Secured 569th Rank in TCS Codevita Season 11</li>
           <li>Solved Total 600+ Problems including all coding platforms</li>
@@ -102,19 +105,26 @@ function Resume() {
           <li>Achieved a Contest Rating of 1604 on Leetcode, ranking in the top 21% of users</li>
           <li>Global Ranking of 115,080/538,088 in Leetcode</li>
         </ul>
-        <h2 style={{color: 'white'}}>Certifications</h2>
+        <h2 style={{color: 'white', paddingTop:'1rem'}}>Certifications</h2>
         <ul>
           <li>CSS, JavaScript And Python Complete Course (Udemy)</li>
           <li>JavaScript (Infytq)</li>
           <li>Programming Fundamentals Using Python (Part 1 & Part 2) (Infytq)</li>
           <li>Object Oriented Programming Using Python (Infytq)</li>
         </ul>
-        <h2 style={{color: 'white'}}>Language Known</h2>
+        <h2 style={{color: 'white', paddingTop:'1rem'}}>Language Known</h2>
         <ul>
           <li>English - Professional Proficiency</li>
           <li>Hindi - Native / Bilingual Proficiency</li>
         </ul>
           </div>
+          <div className="paragraph-button-container">
+              <div className="button">
+                  <a href="https://drive.google.com/file/d/1FqBPHABjh1xD6eAsw8hIJ3ksjmP5j3mv/view" target='_blank' style={{ textDecoration: "none", color: "inherit" }}>
+                  <h5 className="button-text">PDF View <i class="fa-solid fa-arrow-right"></i> </h5> 
+                  </a>
+              </div>
+            </div>
         </div>
       </div>
     </>
